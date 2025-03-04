565c0017d24ed8acd52a9349fd1187796741b0f3
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2025-03-04 18:58:58 -0300

    finishing all exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let position = parseInt(number);
+    return position < 0 ? "OOPS"
+    : position < 1 ? 0
+    : position <= 2 ? 1
+    : fibonacci(position - 1) + fibonacci(position - 2)
 };
-
+let result = fibonacci("4");
+console.log(result);
 // Do not edit below this line
 module.exports = fibonacci;
