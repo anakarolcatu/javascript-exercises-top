32bd8997d566677bb55e201f35751c510a368acc
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2025-03-04 16:42:21 -0300

    add: resolution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    //first transform in array, use split
+    console.log(word);
+    let cleanWord = word
+        .toLowerCase()
+        .replace(/[\s\p{P}]/gu, "");
 
+    let reverse = cleanWord
+        .split('')
+        .reverse()
+        .join('');
+    console.log(reverse);
+
+    //then compare both
+    return cleanWord === reverse;
 };
-
+console.log(palindromes('Rac ecar!'));
 // Do not edit below this line
 module.exports = palindromes;
